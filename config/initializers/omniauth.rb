334e39089359ecf8050ec401f842d2ee95c079aa
quintonwall <qwall@salesforce.com>
2011-09-09 13:27:01 -0700

    updated

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -3,8 +3,8 @@ require 'forcedotcom'
 # Set the default hostname for omniauth to send callbacks to.
 # seems to be a bug in omniauth that it drops the httpS
 # this still exists in 0.2.0
-OmniAuth.config.full_host = 'https://localhost:3000'
-#OmniAuth.config.full_host = 'https://rhatter.heroku.com'
+#OmniAuth.config.full_host = 'https://localhost:3000'
+OmniAuth.config.full_host = 'https://rhatter.heroku.com'
 
 module OmniAuth
   module Strategies
@@ -15,6 +15,6 @@ end
 
 
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :forcedotcom, '3MVG9CVKiXR7Ri5oh_84IylskqO10nW9n6rcGnbQeJ7Z0K91Uzz9TOwukCMYeGlvUsCWlr4ltIjM52iaPf8ON', '8195020619553807815'
+  provider :forcedotcom, '3MVG99OxTyEMCQ3jIW9bdxrL5aCaG9w7u2AC0RqGejjqf50NfYA_iwP5H84fKT9oCD9.DXzagehMBaCGU7cSA', '9153528620843249482'
   
 end
