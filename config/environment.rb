24709f6dbd785cff28012ddbab09179667830e22
quintonwall <qwall@salesforce.com>
2012-01-18 09:10:06 -0800

    updated api version

--- config/environment.rb
+++ config/environment.rb
@@ -4,11 +4,11 @@ require File.expand_path('../application', __FILE__)
 # Initialize the rails application
 OmniauthDemo::Application.initialize!
 
-# store the json output of chatter requess
+# store the json output of chatter requests
 CHATTER_LOGGER = Logger.new("#{RAILS_ROOT}/log/chatter.log")
 CHATTER_LOGGER.level = Logger::DEBUG
 
 # Set our instance URL for Force.com
 #ENV['sfdc_instance_url'] = 'https://na6.salesforce.com'
-#Must be 22 or greater for Chatter REST API
-ENV['sfdc_api_version'] = '22.0'
+#Must be 23 or greater for Chatter REST API
+ENV['sfdc_api_version'] = '23.0'
