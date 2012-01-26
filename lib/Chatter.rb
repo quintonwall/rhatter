84ee712df7707dfa6c868da98760efd0fa920884
quintonwall <qwall@salesforce.com>
2012-01-25 16:11:37 -0800

    updated to current api

--- lib/Chatter.rb
+++ lib/Chatter.rb
@@ -73,7 +73,7 @@ class Chatter
          options.merge!( :body => { :body => { :messageSegments => [
                                      {
                                        :type => "Text",
-                                       :text => comment
+                                       :text => comment.body
                                      }
                                    ]}
                                   }.to_json
